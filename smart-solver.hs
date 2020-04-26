03c264c4a2666bb43573bd7bef25901b24d7725c
Kate Sanders <SandersKM@Hendrix.edu>
2020-04-26 14:02:52 -0500

    single and fixed functions, reducing not working yet

--- smart-solver.hs
+++ smart-solver.hs
@@ -40,6 +40,38 @@ choose e = if blank e then cellvals else [e]
 choices :: Board -> Matrix Choices
 choices = map (map choose)
 
+-- tests whether the argument is a singleton list
+single :: [a] -> Bool
+single [] = False
+single (x:[]) = True
+single (x : xs) = False
+
+-- If there is only one option, it sets it to that
+fixed :: [Choices] -> Choices
+fixed xs = concat (filter single xs)
+
+-- Do we want to remove the characters in fxd from evey one of our choices?
+-- Ask Dr. Yorgey
+delete :: Choices -> [Choices] -> [Choices]
+delete _ [] = []
+delete fxd (x : xs) = if x == fxd then xs else (x : delete fxd xs)
+-- *Main> delete "c" ["c", "e", "r"]
+-- ["e","r"]
+-- *Main> delete "e" ["c", "e", "r"]
+-- ["c","r"]
+
+
+-- ask Dr. Yorgey what's wrong lol
+remove fxd cs = if single cs then cs else delete fxd cs
+
+-- Takes in a list of choices, css
+-- finds the fixed elements in that list of choices,
+-- for each Choices in the list of Choices,
+-- The fixed elements will be removed
+reduce :: [Choices] -> [Choices]
+reduce css = map (remove (fixed css)) css
+
+
 cp :: [[a]] -> [[a]]
 cp [] = [[]]
 cp (xs : xss) = [x : ys | x <- xs, ys <- cp xss]
@@ -49,6 +81,9 @@ mcp xs = cp (map cp xs)
 
 sudoku :: Board -> [Board]
 sudoku b = filter correct (mcp (choices b))
+
+
+
 
 
 boardsize = 4
